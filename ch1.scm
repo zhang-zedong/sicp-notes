85272123cfaeb4e7644c40ce0d1d841d482c0414
zhang-zedong <lycczzd@gmail.com>
2020-12-16 13:48:37 +0800

    add exercise 1.7

--- ch1.scm
+++ ch1.scm
@@ -17,20 +17,25 @@
 (define (sum-special a b c)
     (+ (square (max a b)) (square (max (min a b) c) ) ))
 
-; 1.1.7 Example: Square Roots by Newton's Method
-(define (sqrt-iter guess x)
-    (if (good-enough? guess x)
+
+; 1.1.7 Example
+; Changed the program to the answer of Exercise 1.7.
+(define (sqrt-iter guess last x)
+    (if (good-enough? guess last)
         guess
-        (sqrt-iter (improve guess x) x)))
+        (sqrt-iter (improve guess x) guess x)))
     
-(define (good-enough? guess x)
-    (if (< (abs (- (square guess) x)) 0.0001) #t #f) )
+;(define (good-enough? guess x)
+;    (if (< (abs (- (square guess) x)) 0.001) #t #f) )
+(define (good-enough? guess last)
+    (if (< (abs (- guess last)) 0.001) #t #f))
 
 (define (improve guess x)
     (/ (+ x (square guess)) (* 2 guess) ) )
 
+; 第一次只要让这个1.0 不通过good-enough？就好，所以随意将第二个参数写10
 (define (sqrt x)
-    (sqrt-iter 1.0 x))
+    (sqrt-iter 1.0 10 x))
 
 (define (abs x)
     (if (< x 0) (- x) x))
