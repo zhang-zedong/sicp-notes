210d10de4c14d25b49feb0e4d1d6daa128b5b855
zhang-zedong <lycczzd@gmail.com>
2020-12-26 19:01:21 +0800

    add

---
+++ ex1-29-r.scm
@@ -0,0 +1,30 @@
+; Simpson's rule to calculate integration.
+
+(define (simpson-integrate f a b n)
+    (define h
+        (* (/ (- b a) n)))
+    (define (inc i)
+        (+ i 1))
+    (define (term i)
+        (define g
+            (f (+ a (* i h))))
+        (cond
+            ((or (= i 0) (= i b))
+                g)
+            ((even? i)
+                (* 2 g))
+            (else
+                (* 4 g))))
+    (*  (/ h 3)
+        (sum term 0 inc n)))
+
+(define (sum term a next b)
+    (if (> a b)
+        0
+        (+ (term a)
+           (sum term (next a) next b))))
+
+; (simpson-integrate cube 0 1 100)
+; (simpson-integrate cube 0 1 1000)
+
+; 这道题答案还是不对，不管h用分数还是小数，答案都是错一些。
