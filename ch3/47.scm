efee0f8935791b7a8f2a80df06d3131315a12347
zedong <lycczzd@gmail.com>
2022-11-12 23:12:41 +0800

    test

---
+++ ch3/47.scm
@@ -0,0 +1,37 @@
+#lang sicp
+
+(define (make-serializer)
+  (let ((mutex (make-mutex)))
+    (lambda (p)
+      (define (serialized-p . args)
+        (mutex 'acquire)
+        (let ((val (apply p args)))
+          (mutex 'release)
+          val))
+      serialized-p)))
+
+(define (make-mutex)
+  (let ((cell (list false)))
+    (define (the-mutex m)
+      (cond ((eq? m 'acquire)
+             (if (test-and-set! cell)
+                 (the-mutex 'acquire)))
+            ((eq? m 'release) (clear! cell))))
+    the-mutex))
+(define (clear! cell) (set-car! cell false))
+(define (test-and-set! cell)
+  (if (car cell) true (begin (set-car! cell true) false)))
+
+
+; test for 4.47
+(define (make-semaphore n)
+  (let ((cell (list n)))
+    (define (the-semaphore m)
+      (cond ((eq? m 'acquire)
+             (if (test-and-set-s! cell)
+                 (the-semaphore 'acquire)))
+            ((eq? m 'release) (clear-s! cell))))
+    the-semaphore))
+(define (clear-s! cell) (set-car! cell (+ (car cell) 1)))
+(define (test-and-set-s! cell)
+  (if (<= (car cell) 0) true (begin (set-car! cell (- (car cell) 1)) false)))
